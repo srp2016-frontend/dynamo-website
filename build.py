f4559d70f80118bed533b8f3bcf2bd7a6ea26acb
Ryan Goldstein <ryanisaacg@gmail.com>
2016-06-29 11:30:53 -0400

    Added a sidebar in a separate file

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 with open('main.html', 'r') as file_input:
     contents = file_input.read()
+with open('sidebar.html', 'r') as file_input:
+    sidebar = file_input.read()
 import os
 src = os.listdir('src')
-contents = contents.replace("<!--SCRIPTS GO HERE-->", "\n".join(map(lambda name: "<script src='src/" + name + "'></script>", src)))
+contents = contents.replace("<!--SCRIPTS GO HERE-->", "\n".join(map(lambda name: "<script src='src/" + name + "'></script>", src))).replace("<!--SIDEBAR GOES HERE-->", sidebar)
 open('index.html', 'w').write(contents)
