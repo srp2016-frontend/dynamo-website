20c3690c0b46ed33e2fc682d1022e7341a8798cd
Deleonardis <bdeleona@monmouth.edu>
2016-06-29 15:13:25 -0400

    Added hover behavior

--- src/hover.ts
+++ src/hover.ts
@@ -1,4 +1,8 @@
 ///<reference path='plotting.ts'/>
-function handle_hover_person(person : Person) {
-    console.log(person)
+function handle_hover_person(person : Person) 
+{
+    $("#sidebar").append("First Name: ", person.fName, "<br>");
+    $("#sidebar").append("Last Name:  ", person.lName, "<br>");
+    $("#sidebar").append("Age:      &nbsp", person.age, "<br>");
+    //$("#sidebar").append(person.age);
 }
