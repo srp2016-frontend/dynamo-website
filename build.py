f336e6782086c33782ffaba1175f91d15a5c4327
Ryan Goldstein <ryanisaacg@gmail.com>
2016-06-29 14:29:08 -0400

    Removed the python build system

--- build.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import os
-if not os.path.exists("node_modules"):
-    os.system("npm install")
-if os.name == 'nt':
-    os.system("cd node_modules\\typescript\\bin\\")
-    os.system("tsc ..\\..\\..\\src\\*.ts --outFile bin\\script.js")
-else:
-    os.system("node_modules/typescript/bin/./tsc src/*.ts --outFile bin/script.js")
