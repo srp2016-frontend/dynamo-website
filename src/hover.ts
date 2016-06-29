14cd39b0072fb06a38d55057d8aaab211de72bba
Brian Deleonardis <bdeleonardis1@gmail.com>
2016-06-29 15:21:28 -0400

    With clearing

--- src/hover.ts
+++ src/hover.ts
@@ -1,8 +1,9 @@
 ///<reference path='plotting.ts'/>
 function handle_hover_person(person : Person) 
 {
+    $("#sidebar").empty();
     $("#sidebar").append("First Name: ", person.fName, "<br>");
     $("#sidebar").append("Last Name:  ", person.lName, "<br>");
-    $("#sidebar").append("Age:      &nbsp", person.age, "<br>");
+    $("#sidebar").append("Age: ", person.age, "<br>");
     //$("#sidebar").append(person.age);
 }
