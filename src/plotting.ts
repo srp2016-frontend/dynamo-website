20b4857f88a3b97fc40f0e095747eb356e8dd206
Ryan Goldstein <ryanisaacg@gmail.com>
2016-06-29 13:40:07 -0400

    Created a typescript version of the plotting script

---
+++ src/plotting.ts
@@ -0,0 +1,18 @@
+///<reference path='node.d.ts'/>
+///<reference path='jquery.d.ts'/>
+function plot_points(ctx, point_list) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = "red";
+    for(var i = 0; i < point_list.length; i++) {
+        var point = point_list[i];
+        ctx.beginPath();
+        ctx.arc(point.x, point.y, 6, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+function Point (x, y) {
+    this.x = x;
+    this.y = y;
+}
+var canvas = $('#position-feed')[0];
+plot_points(canvas.getContext('2d'), [new Point(10, 10), new Point(50, 32)])
