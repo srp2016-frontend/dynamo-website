49f84fd0627616890f10e34dfa8a5856bce3f3f6
Ryan Goldstein <ryanisaacg@gmail.com>
2016-06-29 14:56:54 -0400

    Clarified return type of draw

--- src/plotting.ts
+++ src/plotting.ts
@@ -27,7 +27,7 @@ class State
         this.people = people;
     }
 
-    draw(ctx : CanvasRenderingContext2D)
+    draw(ctx : CanvasRenderingContext2D) : void
     {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         ctx.fillStyle = "red";
