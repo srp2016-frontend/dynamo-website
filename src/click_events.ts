a756aee1319f76c66ff762e8328df89dd632a67a
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-07 11:26:16 -0400

    Refactored click events into a seperate file out of the global namespace

---
+++ src/click_events.ts
@@ -0,0 +1,22 @@
+/// <reference path="plotting.ts" />
+
+function setClickEvents(canvas : HTMLCanvasElement, ctx : CanvasRenderingContext2D, state : State) 
+{
+    canvas.onmousedown = (e : MouseEvent) =>
+    {
+        if(e.shiftKey)
+            state.addSelection(state.getPersonAt(e.offsetX, e.offsetY));
+        else
+            state.setSelection(state.getPersonAt(e.offsetX, e.offsetY));
+        state.draw(ctx);
+    }
+
+    canvas.onmousemove = function(e : MouseEvent)
+    {
+        if(!state.hasSelection())
+        {
+            state.setDisplay(state.getPersonAt(e.offsetX, e.offsetY))
+            state.draw(ctx);
+        }
+    }
+}
