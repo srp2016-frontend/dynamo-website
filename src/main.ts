01950c6d53c039c415a1ed7b0bb97b24b9c6ae21
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-27 09:49:54 -0400

    Capitalized athlete

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@
 function main() {
     let canvas = <HTMLCanvasElement>$('#position-feed')[0];
     let ctx = canvas.getContext('2d');
-    let state = new State([new Item(40, 40, 'Brian DeLeonardis', 18, 'Iceland', 'athlete'),new Item(40, 80, 'Jack Dates', 18, 'Haiti', 'athlete'),new Item(40, 120, 'Anthony Fasano', 18, 'Solomon Islands', 'athlete'),new Item(40, 160, 'Anthony Hamill', 18, 'Peru', 'athlete'),new Item(40, 200, 'Brandon Guglielmo', 18, 'Chile', 'athlete'),new Item(40, 240, 'Chase Moran', 18, 'Mozambique', 'athlete'),new Item(40, 280, 'Daniel Collins', 18, 'Bahrain', 'athlete'),new Item(40, 320, 'Kevin DeStefano', 18, 'Nigeria', 'athlete'),new Item(40, 360, 'Matthew Kumar', 18, 'Italy', 'athlete'),new Item(40, 400, 'Ryan Goldstein', 18, 'Mozambique', 'athlete'),new Item(40, 440, 'Tina Lu', 18, 'San Marino', 'athlete'),])
+    let state = new State([new Item(40, 40, 'Brian DeLeonardis', 18, 'Bhutan', 'Athlete'),new Item(40, 80, 'Jack Dates', 18, 'Tajikistan', 'Athlete'),new Item(40, 120, 'Anthony Fasano', 18, 'Equatorial Guinea', 'Athlete'),new Item(40, 160, 'Anthony Hamill', 18, 'Bermuda', 'Athlete'),new Item(40, 200, 'Brandon Guglielmo', 18, 'The Bahamas', 'Athlete'),new Item(40, 240, 'Chase Moran', 18, 'Italy', 'Athlete'),new Item(40, 280, 'Daniel Collins', 18, 'Dominica', 'Athlete'),new Item(40, 320, 'Kevin DeStefano', 18, 'Saint Kitts and Nevis', 'Athlete'),new Item(40, 360, 'Matthew Kumar', 18, 'Palau', 'Athlete'),new Item(40, 400, 'Ryan Goldstein', 18, 'Cambodia', 'Athlete'),new Item(40, 440, 'Tina Lu', 18, 'Cayman Islands', 'Athlete'),])
     let bridge = new Bridge();
     let items : string[];
     let count = 0;
