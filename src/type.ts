c1687f202c8e291856c05ddfec79be140243b7b3
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-02 10:15:13 -0400

    Fixed too-wide replace of menu items

--- src/type.ts
+++ src/type.ts
@@ -11,7 +11,7 @@ function changeSettings()
     $("#title").append(": " + type)
     if(type === "Water")
     {
-        $("#menu").html('<input type="checkbox" id="video-check" check>Video</input><br/><input type="checkbox" id ="tech-check">Technology</input><br/>')
+        $("#menu-boxes").html('<input type="checkbox" id="video-check" check>Video</input><br/><input type="checkbox" id ="tech-check">Technology</input><br/>')
         ["selection-panel", "position-feed", "alert-panel", "manifest-panel"].map(item => $("#" + item).remove())
         $("#video-feed").css("float", "none")
         $("#content").css("text-align", "center")
