bcaab1b3d59638f9b2293eb2a6474ea3eb3d742f
Brian Deleonardis <bdeleonardis1@gmail.com>
2016-08-03 13:21:17 -0400

    Changed video.

--- src/type.ts
+++ src/type.ts
@@ -22,6 +22,8 @@ function changeSettings()
         $("#video-feed").css("float", "none")
         $("#content").css("text-align", "center")
         $("#water-tech").css("visibility", "visible")
+        $("#video-feed video source").attr('src', "../video/likeABausVideo.mp4") 
+        $("#video-feed video")[0].load()
     }
     else if(type === "Triathlon")
     {
