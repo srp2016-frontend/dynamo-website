b0e2ee1490262332fc8f126f502a083c42254395
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-03 11:51:05 -0400

    Set the active shooter background to a blue field

--- src/type.ts
+++ src/type.ts
@@ -35,5 +35,6 @@ function changeSettings()
         '<input type = "checkbox" id = "tech-check">Technology</input><br/>') 
         $("#video-feed").remove()
         $("#shooter-tech").css("visibility", "visible");    
+        $("#map-background").attr("src", "../img/blueprint.png")
     }
 }
