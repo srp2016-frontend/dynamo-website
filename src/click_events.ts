39b06ddcec81cbb21752c250ca95dbc384c5005a
Ryan Goldstein <ryanisaacg@gmail.com>
2016-08-03 14:09:53 -0400

    Added a zoom item

--- src/click_events.ts
+++ src/click_events.ts
@@ -4,11 +4,14 @@ function setClickEvents(canvas : HTMLCanvasElement, ctx : CanvasRenderingContext
 {
     canvas.onmousedown = (e : MouseEvent) =>
     {
-        if(e.shiftKey)
-            state.addSelection(state.getItemAt(e.offsetX, e.offsetY));
-        else
-            state.setSelection(state.getItemAt(e.offsetX, e.offsetY));
-        state.draw(ctx);
+        if(e.button === 1)
+        {
+            if(e.shiftKey)
+                state.addSelection(state.getItemAt(e.offsetX, e.offsetY));
+            else
+                state.setSelection(state.getItemAt(e.offsetX, e.offsetY));
+            state.draw(ctx);
+        }
     }
 
     canvas.onmousemove = function(e : MouseEvent)
@@ -18,5 +21,11 @@ function setClickEvents(canvas : HTMLCanvasElement, ctx : CanvasRenderingContext
             state.setDisplay(state.getItemAt(e.offsetX, e.offsetY))
             state.draw(ctx);
         }
+        let zoomCanvas = <HTMLCanvasElement>$("#zoom")[0]
+        let zCtx = zoomCanvas.getContext('2d')
+        let offset = $(canvas).offset()
+        let x = e.pageX - offset.left
+        let y = e.pageY - offset.top
+        zCtx.drawImage(canvas, x - 62, y - 62, 125, 125, 0, 0, 250, 250)
     }
 }
